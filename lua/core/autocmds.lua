9aff18f7d3059cf493ddfcbaf7a86fdc294e2cfa
Setiadi Prawiryo <setiadi@esb.co.id>
2024-03-23 17:59:57 +0700

    fix: update core and plugin options

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -81,7 +81,3 @@ autocmd({ "BufWritePre" }, {
     vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
-
--- Add binaries installed by mason.nvim to path
--- local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
--- vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
